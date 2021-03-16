c812e80baf6de3e9e6040f0ab664f8fa518bf21a
iimabdulkarim <iimakarim@student.telkomuniversity.ac.id>
2021-03-16 14:24:51 +0700

    Membuat fragment lihat saran

--- app/src/main/java/org/d3if0018/hitungbmi/MainActivity.kt
+++ app/src/main/java/org/d3if0018/hitungbmi/MainActivity.kt
@@ -2,12 +2,6 @@ package org.d3if0018.hitungbmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.text.TextUtils
-import android.util.Log
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
-import org.d3if0018.hitungbmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
