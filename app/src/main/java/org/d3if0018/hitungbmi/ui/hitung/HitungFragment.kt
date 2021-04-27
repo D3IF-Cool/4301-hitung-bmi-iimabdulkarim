5dea3ccd869715a3c3c7ed8ea04a44c73c3290ee
iimabdulkarim <iimakarim@student.telkomuniversity.ac.id>
2021-04-27 19:43:15 +0700

    Memperbaiki navigasi dengan view model

--- app/src/main/java/org/d3if0018/hitungbmi/ui/hitung/HitungFragment.kt
+++ app/src/main/java/org/d3if0018/hitungbmi/ui/hitung/HitungFragment.kt
@@ -1,4 +1,4 @@
-package org.d3if0018.hitungbmi.ui
+package org.d3if0018.hitungbmi.ui.hitung
 
 import android.content.Intent
 import android.os.Bundle
@@ -18,7 +18,6 @@ class HitungFragment : Fragment() {
 
     private val viewModel: HitungViewModel by viewModels()
     private lateinit var binding: FragmentHitungBinding
-    private lateinit var kategoriBmi: KategoriBmi
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
@@ -39,12 +38,7 @@ class HitungFragment : Fragment() {
         binding = FragmentHitungBinding.inflate(
             layoutInflater, container, false)
         binding.button.setOnClickListener { hitungBmi() }
-        binding.saranButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(
-                HitungFragmentDirections.
-                        actionHitungFragmentToSaranFragment(kategoriBmi)
-            )
-        }
+        binding.saranButton.setOnClickListener { viewModel.mulaiNavigasi() }
         binding.shareButton.setOnClickListener { shareData() }
         setHasOptionsMenu(true)
         return binding.root
@@ -52,6 +46,12 @@ class HitungFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getNavigasi().observe(viewLifecycleOwner, {
+            if (it == null) return@observe
+            findNavController().navigate(HitungFragmentDirections.actionHitungFragmentToSaranFragment(it))
+            viewModel.selesaiNavigasi()
+        })
 
         viewModel.getHasilBmi().observe(viewLifecycleOwner, {
             if (it == null) return@observe
